d852ae3f8328f40ca25b774ccbae10f4be1bd7c5
Ryan Graue <grauer@spu.edu>
2021-06-09 19:04:48 -0700

    get and post

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-//require('dotenv').config()
 
 var createError = require('http-errors');
 var express = require('express');
@@ -6,6 +5,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// initializes mongoose connection singleton
 let db= require('./db')
 
 let bookRouter = require('./routes/books')
--- routes/books.js
+++ routes/books.js
@@ -3,10 +3,11 @@ let router = express.Router();
 let bookSchema = require('../models/book')
 
 router.get('/', (request, response, next)=>{
-    let title = request['title']
+    let title = request.query['title']
     if (title){
+        console.log(title);
         bookSchema
-            .find({"title": title})
+            .find({'title': title})
             .exec( (error, book) =>{
                if (error){
                    response.send({"error": error});
@@ -26,5 +27,46 @@ router.get('/', (request, response, next)=>{
             });
     }
 });
+
+router.get('/:id', (request, response, next)=>{
+    bookSchema.findById({"_id": request.params.id}, (error, result) =>{
+        if (error){
+            response.status(500).send(error);
+        } else if (result){
+            response.send(result);
+        } else {
+            response.status(404).send({"id": request.params.id, "error": "Not Found"});
+        }
+    });
+});
+
+router.post('/', (request, response, next) =>{
+    let bodyJson = request.body;
+    console.log(request.body)
+    if (!bodyJson.title || !bodyJson.author){
+        HandleError(response, 'Missing Information', 'Form Data Missing', 500);
+    } else {
+        let book = new bookSchema({
+            title:bodyJson.title,
+            description:bodyJson.description || "",
+            year: bodyJson.year || 2021,
+            author: bodyJson.author,
+            hardCover: bodyJson.hardCover || true,
+            price: bodyJson.price || 0.0
+        })
+        book.save((error)=>{
+          if (error){
+              response.send({"error" : error})
+          }  else{
+              response.send({"id":book._id})
+          }
+        })
+    }
+});
+
+function HandleError(response, reason, message, code){
+    console.log('ERROR: ' + reason);
+    response.status(code || 500).json({"error": message});
+}
 
 module.exports = router;
